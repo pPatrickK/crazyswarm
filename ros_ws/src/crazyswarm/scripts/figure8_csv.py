7987eb2433693f2a4a90834cc59f669bc1d98d08
pPatrickK <patrick.klokowski@udo.edu.de>
2018-05-25 13:32:58 +0200

    set ti current version

--- ros_ws/src/crazyswarm/scripts/figure8_csv.py
+++ ros_ws/src/crazyswarm/scripts/figure8_csv.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     allcfs = swarm.allcfs
 
     traj1 = uav_trajectory.Trajectory()
-    traj1.loadcsv("figure8.csv")
+    traj1.loadcsv("test.csv")
 
     TRIALS = 1
     TIMESCALE = 1.0
@@ -19,18 +19,11 @@ if __name__ == "__main__":
         for cf in allcfs.crazyflies:
             cf.uploadTrajectory(0, 0, traj1)
 
-        allcfs.takeoff(targetHeight=1.0, duration=2.0)
+        allcfs.takeoff(targetHeight=0.3, duration=2.0)
         timeHelper.sleep(2.5)
-        for cf in allcfs.crazyflies:
-            pos = np.array(cf.initialPosition) + np.array([0, 0, 1.0])
-            cf.goTo(pos, 0, 2.0)
-        timeHelper.sleep(2.5)
-
-        allcfs.startTrajectory(0, timescale=TIMESCALE)
-        timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
-        allcfs.startTrajectory(0, timescale=TIMESCALE, reverse=True)
-        timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
+	for i in range(3):
+            allcfs.startTrajectory(0, timescale=TIMESCALE)
+            timeHelper.sleep(traj1.duration * TIMESCALE + 2.0)
 
         allcfs.land(targetHeight=0.06, duration=2.0)
         timeHelper.sleep(3.0)
-
